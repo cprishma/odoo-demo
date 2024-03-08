5966e4d8c7df7b9c6745a87febda0769778f3b81
cprishma <rishmacp2022@gmail.com>
2024-03-08 12:38:32 +0530

    your_message

--- first_module/__manifest__.py
+++ first_module/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "first_module",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Hiii""",
 
     'description': """
         Long description of module's purposeses
